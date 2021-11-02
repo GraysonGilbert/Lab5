189dd8b72690b19692d64bde45330e4019c79730
Grayson Gilbert <91498658+GraysonGilbert@users.noreply.github.com>
2021-11-02 04:41:15 +0000

    code

--- StepperClass.py
+++ StepperClass.py
@@ -7,24 +7,20 @@ pins = [18,21,22,23] # controller inputs: in1, in2, in3, in4
 for pin in pins:
   GPIO.setup(pin, GPIO.OUT, initial=0)
 
+# Define the pin sequence for counter-clockwise motion, noting that
+# two adjacent phases must be actuated together before stepping to
+# a new phase so that the rotor is pulled in the right direction:
 sequence = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
-      [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+        [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
 
-state = 0
+state = 0 #current positon in the stator sequence
+
 
 def delay_us(tus): # use microseconds to improve time resolution
   endTime = time.time() + float(tus)/ float(1E6)
   while time.time() < endTime:
     pass
-
 class Stepper:
-
-  def __init__(motor,pins):
-    motor.pins = pins
-    motor.sequence = sequence
-    motor.state = state 
-
-
 
   def halfstep(dir):
     #dir = +/- 1 for cw or cw respectfully
@@ -38,29 +34,15 @@ class Stepper:
     for pin in range(4):    # 4 pins that need to be energized
       GPIO.output(pins[pin],sequence[state][pin])
     delay_us(1000)
-  
-
+    
   def turnSteps(motor, steps, dir):
     #move the actuation sequence a given number of half steps
     for step in range(steps):
       motor.halfstep(dir)
 
-
-
-
-
-  #def goAngle():
-    #mve a specified angle, taking the shortest path at a user defined speed
-
-  #def zero():
-    #Turn the motor until the photoresistor is occluded by the cardboard piece
-
-
-  
-myStepper = Stepper(pins)
-
+myStepper = Stepper()
 try:
-  myStepper.turnSteps(4096,1)
+  turnSteps(4096,1)
 except:
   pass
 GPIO.cleanup() 
